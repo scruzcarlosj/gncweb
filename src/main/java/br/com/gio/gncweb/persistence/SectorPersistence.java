980c0f6c46bda5ec23dcfe8525d209ac4a350f80
giorne <giorne.souza@gmail.com>
2014-10-04 22:53:27 -0300

    Alteração de Layout e mudança da query findByName para uso do Like

--- src/main/java/br/com/gio/gncweb/persistence/SectorPersistence.java
+++ src/main/java/br/com/gio/gncweb/persistence/SectorPersistence.java
@@ -14,11 +14,11 @@ public class SectorPersistence extends BasePersistence<Long, Sector> {
 	public List<Sector> findByName(String name){
 		StringBuilder sb = new StringBuilder();
 		sb.append("select sector from Sector sector ");
-		sb.append("where sector.name = :name ");
+		sb.append("where sector.name like :name ");
 		sb.append("order by sector.name ");
 		
 		TypedQuery<Sector> query = getEntityManager().createQuery(sb.toString(), Sector.class);
-		query.setParameter("name", name);
+		query.setParameter("name", name != null ? '%' + name + '%': name);
 		
 		return query.getResultList();
 	}
